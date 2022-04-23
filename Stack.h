cf232f08a736937732e6be455bf6d356791f843f
Aleyna Alper <67961314+Aleynaalper@users.noreply.github.com>
2022-04-23 14:40:32 +0300

    Example Files

--- Stack.h
+++ Stack.h
@@ -1,6 +1,6 @@
-/*Function templates are special functions that can operate
-with generic types. This allows us to create a function
-template whose functionality can be adapted to more than
+/*Function templates are special functions that can operate 
+with generic types. This allows us to create a function 
+template whose functionality can be adapted to more than 
 one type or class without repeating the entire code for each type.*/
 
 /*To use this function template we use the following format for the function call:
@@ -8,65 +8,28 @@ one type or class without repeating the entire code for each type.*/
 function_name <type> (parameters);
 
 */
-#ifndef STACK_H
-#define STACK_H
-#include "Package.h"
-#include <iostream>
-using namespace std;
 
 template <typename T>
 class Stack {
 	public:
-	Stack(){
-
-	}
-	~Stack(){
-
-	}
-	bool isEmpty() const{
-		if(_head==NULL){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
+	Stack();
+	~Stack();
+	bool isEmpty() const;
 	int size() const;
-
+	
 	//Push: Place item on top of the stack
-	void push(const T& newItem){
-		ListNode* newuser=new ListNode();
-			newuser->item=newItem;
-			newuser->next=_head;
-
-			_head=newuser;
-	}
+	void push(const T& newItem);
 	//Top: Take a look at the topmost item without removing it
-	void getTop(T& stackTop) const{
-		if(_head!=NULL){
-				stackTop=_head->item;
-				}
-				else{
-					throw "StackException";
-				}
-	}
+	void getTop(T& stackTop) const;
 	//Pop: Remove item from the top of the stack
-	void pop(){
-		if(_head==NULL)
-		   cout<<"Stack is empty"<<endl;
-	    else{
-                ListNode *temp = _head;
-                _head = _head->next;
-                free(temp);
-	    }
-	}
-
+	void pop();
 	private:
 	struct ListNode {
 		T item;
 		ListNode* next;
 		};
-	ListNode* _head=NULL;
+	ListNode* _head;
 	int _size;
 };
-#endif
+	
+	
